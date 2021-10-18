e3e5904f27ace746a2c96d56c185e51006f8d36a
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-10-18 18:54:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let Latte = 26,
+  nl = "Latte";
+let Americano = 20,
+  na = "Americano";
+let Capuchino = 23,
+  nc = "Capuchinoe";
+let Chocolate = 27,
+  nch = "Latte";
+
+function drink(name) {
+  document.write(name + "<br>");
+}
+function receip(coffee) {
+  document.write("Sum:" + coffee + "grn");
+}
