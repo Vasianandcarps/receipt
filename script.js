6486d314acd982763f9990ecd0f85bf8806a2def
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-10-20 14:40:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ function order() {
     let order = prompt("Input your order");
     switch (order) {
       case "Latte":
-        // nl = "Latte";
-        document.write("<br>" + 26 + "grn");
+        nl = "Latte";
+        document.write(nl + "<br>" + 26 + "grn");
         break;
       case "Americano":
-        // na = "Americano";
-        document.write("<br>" + 20 + "grn");
+        na = "Americano";
+        document.write(na + "<br>" + 20 + "grn");
         break;
       case "Capuchino":
-        // nc = "Capuchino";
-        document.write("<br>" + 23 + "grn");
+        nc = "Capuchino";
+        document.write(nc + "<br>" + 23 + "grn");
         break;
       case "Chocolate":
-        // nch = "Chocolate";
-        document.write("<br>" + 27 + "grn");
+        nch = "Chocolate";
+        document.write(nch + "<br>" + 27 + "grn");
         break;
       default:
         alert("Try again");
