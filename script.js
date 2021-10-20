9f5b106ae4f5d087df385cbee6cb90489aa45f51
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-10-20 14:38:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 function order() {
-  let order = prompt("Input your order");
-  switch (order) {
-    case "Latte":
-      nl = "Latte";
-      document.write(nl + "<br>" + 26 + "grn");
-      break;
-    case "Americano":
-      na = "Americano";
-      document.write(na + "<br>" + 20 + "grn");
-      break;
-    case "Capuchinoe":
-      nc = "Capuchinoe";
-      document.write(nc + "<br>" + 23 + "grn");
-      break;
-    case "Chocolate":
-      nch = "Chocolate";
-      document.write(nch + "<br>" + 27 + "grn");
-      break;
-    default:
-      alert("Try again");
+  n = prompt("how many drinks do you want?");
+  for (let i = 0; i < n; i++) {
+    let order = prompt("Input your order");
+    switch (order) {
+      case "Latte":
+        // nl = "Latte";
+        document.write("<br>" + 26 + "grn");
+        break;
+      case "Americano":
+        // na = "Americano";
+        document.write("<br>" + 20 + "grn");
+        break;
+      case "Capuchino":
+        // nc = "Capuchino";
+        document.write("<br>" + 23 + "grn");
+        break;
+      case "Chocolate":
+        // nch = "Chocolate";
+        document.write("<br>" + 27 + "grn");
+        break;
+      default:
+        alert("Try again");
+    }
   }
 }
-}
+
